0a528ab6b17aadffd040bfef393985f3c82d57b5
himgod <himgod@sina.com>
2015-01-08 16:58:19 +0800

    update pie.js,add function drawPolar()

--- js/pie.js
+++ js/pie.js
@@ -22,17 +22,17 @@ function pieData(score_arg)
 
 	if(this.score > 90)
 	{
-		this.innersize = "35%";
+		this.innersize = "80%";
 		this.plotsize = "110%";
 	}
 	else if(this.score > 60)
 	{
-		this.innersize = "35%";
+		this.innersize = "50%";
 		this.plotsize = "80%";
 	}
 	else
 	{
-		this.innersize = "35%";
+		this.innersize = "40%";
 		this.plotsize = "70%";
 	}
 	for (var i = 0; i < this.categories.length; i++) {
@@ -76,6 +76,9 @@ function drawPie(div_id)
                         	enabled: true
                    			}
 				        }
+				},
+				credits:{
+					enabled: false
 				},
 				tooltip: {
 				    //valueSuffix: '%',
@@ -178,4 +181,140 @@ function num()
 		}
 	   
 	}
+}
+
+function drawPolar(div_id)
+{
+	score_tmp = Math.floor((Math.random()*100+1));
+	var polarData =[];
+	for(var i = 0 ; i < 5;i++)
+	{
+		polarData.push(Math.floor((Math.random()*100+40)));
+	}
+	$(function () {
+    // Parse the data from an inline table using the Highcharts Data plugin
+			$(div_id).highcharts({
+				chart: {
+					polar: true,
+				},
+				
+				title: {
+					text: 'Wind rose for South Shore Met Station, Oregon'
+				},
+				
+				subtitle: {
+					text: 'Source: or.water.usgs.gov'
+				},
+				pane: {
+					size:"100%"
+				},
+				
+				legend: {
+					reversed: true,
+					align: 'right',
+					verticalAlign: 'top',
+					y: 100,
+					layout: 'vertical'
+				},
+				
+				xAxis: {
+					categories:['','','','',''],
+					//tickmarkPlacement: 'on',
+					enabled:false
+				},
+					
+				yAxis: {
+					min: 0,
+					endOnTick: false,
+					//showLastLabel: true,
+					//reversedStacks: true,
+					title: {
+						//text: 'Frequency (%)'
+					},
+					labels: {
+						formatter: function () {
+							//return this.value + '%';
+						}
+					}
+				},
+				
+				tooltip: {
+					valueSuffix: '%',
+					//followPointer: true
+				},
+					
+				plotOptions: {
+					series: {
+						stacking: 'normal',
+						shadow: false,
+						//grouping:true,
+						groupPadding: 0,
+						pointPlacement: 'between',
+						//pointStart: 20,
+						//pointInterval: 0,
+						colorByPoint: true,
+						dataLabels:{
+							crop:true
+						}
+					},
+					pie: {
+						shadow: false,
+						center: ['50%', '50%'],
+						dataLabels: {
+                        	enabled: false
+                   			}
+					},
+					column: {
+						pointPadding: 0,
+						groupPadding: 0
+					}
+				},
+				series: [{
+					type: 'column',
+					data: polarData,          
+					colors:["#DD1D00","#FB6362","#F8A20F","#26881D","#3CA7EF"],
+					animation: {
+			            duration: 2000
+			            }
+				},
+				{
+                name:'100',
+                type:'pie',
+                data:[10],
+                colors:["#FFFFFF","#FB6362","#F8A20F","#26881D","#3CA7EF"],
+                size:'25%',
+				animation: {
+			            duration: 2000
+			            }
+				}]
+			},
+			function(chart)
+			{
+			 //var test = chart.renderer.text('<div class="test" style="display:block">'+99+'</div>', chart.chartWidth/2-80, chart.chartHeight/2+48)
+					        // .attr({zIndex:3})
+					        // .css({
+					           //  color: '#4512A7',
+					            // fontSize: '50px'
+					        // })
+					        // .add();
+				width = chart.chartWidth/2-88;
+				height = chart.chartHeight/2+48;
+			}
+			);
+		if( score_tmp == 100 )
+		{
+			$("#view").css("left",width);
+			$("#view").css("top",height);
+		}
+		$("#view").css("left",width+13);
+		$("#view").css("top",height-28);
+		$("#view").css("width",54);
+		$("#view").css("height",52);
+		$("#view").css("position","absolute");
+		$("#view").css("font-size","65px");
+		$("#view").css("color","#4512A7");
+		$("#view").html("00");
+		timerID = setInterval("num()", 2500/score_tmp);
+		
+		});
 }
